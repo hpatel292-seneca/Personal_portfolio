dcf198ba15ffa39c39187cf863131db496db0da0
Harshil Patel <26harshilpatel11@gmail.com>
2022-12-27 06:13:56 -0500

    Update the final profile picture and add the cards layout for the work page

--- my_app/src/container/work/Work.jsx
+++ my_app/src/container/work/Work.jsx
@@ -1,29 +1,27 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import "../../app.css"
 import "./Work.css"
+import { images } from '../../constant/index'
 import AppWarp from '../../Higher_order_component/AppWapper'
 import { motion, useAnimationControls } from "framer-motion";
+import { AiFillEye, AiFillGithub } from 'react-icons/ai';
+import data from './data.jsx'
 function Work() {
 
   const controls = useAnimationControls();
-  const data = [
-    { title: "web app", content: [1, 2, 3, 4, 5] },
-    { title: "Flask app", content: [6, 2, 3, 4, 5] },
-    { title: "C++ app", content: [7, 2, 3, 4, 5] },
-    { title: "React app", content: [8, 2, 3, 4, 5] }
-  ];
+  // const data = Data
   const [selectedIndex, setSelectedIndex] = useState(0);
   async function handleClick(index) {
     await controls.start({
       y: [0, 100],
       opacity: [1, 0],
-      transition: { duration: 1 }
+      transition: { duration: 0.5 }
     });
     setSelectedIndex(index);
     await controls.start({
       y: [100, 0],
       opacity: [0, 1],
-      transition: { duration: 1 }
+      transition: { duration: 0.5 }
     });
   }
   const selectedData = data[selectedIndex].content;
@@ -53,18 +51,67 @@ function Work() {
         </div>
 
         <motion.div
-          className="app__work-content"
+          className="app__work-content app__flex"
           animate={controls}
-          transition={{ duration: 2 }}
+          transition={{ duration: 2, staggerChildren: 0.5}}
         >
           {selectedData.map((item, index) => (
-            <div className="app__work-content-item">{item}</div>
+            // <div className="app__work-content-item">
+            //   <img src={item.imgURL}/>
+            //   <p>"Web App"</p>
+            //   <h5>{item.title}</h5>
+            //   <p>{item.text}</p>
+            // </div>
+            <div className="app__work-item app__flex" key={index}>
+              <div
+                className="app__work-img app__flex"
+              >
+                <img src={item.imgURL} alt={item.name} />
+
+                <motion.div
+                  whileHover={{ opacity: [0, 1] }}
+                  transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
+                  className="app__work-hover app__flex"
+                >
+                  <a href={item.github} target="_blank" rel="noreferrer">
+
+                    <motion.div
+                      whileInView={{ scale: [0, 1] }}
+                      whileHover={{ scale: [1, 0.90] }}
+                      transition={{ duration: 0.25 }}
+                      className="app__flex"
+                    >
+                      <AiFillEye />
+                    </motion.div>
+                  </a>
+                  <a href={item.live_site} target="_blank" rel="noreferrer">
+                    <motion.div
+                      whileInView={{ scale: [0, 1] }}
+                      whileHover={{ scale: [1, 0.90] }}
+                      transition={{ duration: 0.25 }}
+                      className="app__flex"
+                    >
+                      <AiFillGithub />
+                    </motion.div>
+                  </a>
+                </motion.div>
+              </div>
+
+              <div className="app__work-content app__flex">
+                <h5 className="bold-text">{item.title}</h5>
+                <p className="p-text" style={{ marginTop: 10 }}>{item.text}</p>
+
+                <div className="app__work-tag app__flex">
+                  <p className="p-text">"Web App"</p>
+                </div>
+              </div>
+            </div>
           ))}
         </motion.div>
       </motion.div>
 
     </div>
-      // </div >
+    // </div >
   )
 }
 
---
+++ my_app/src/container/work/data.jsx
@@ -0,0 +1,30 @@
+import { images } from '../../constant/index'
+
+ const data = [
+    { title: "Javascript", content: [{title: "Web Application", text: "This a web application i have made using react.js", imgURL: images.about01, github: "https://github.com/", live_site: "https://github.com/"},
+                                  {title: "Web Application", text: "This a web application i have made using react.js", imgURL: images.about02, github: "https://github.com/", live_site: "https://github.com/"},
+                                  {title: "Web Application", text: "This a web application i have made using react.js", imgURL: images.about03, github: "https://github.com/", live_site: "https://github.com/"},
+                                  {title: "Web Application", text: "This a web application i have made using react.js", imgURL: images.about04, github: "https://github.com/", live_site: "https://github.com/"},
+                                  {title: "Web Application", text: "This a web application i have made using react.js", imgURL: images.about01, github: "https://github.com/", live_site: "https://github.com/"}
+] },
+{ title: "Python", content: [{title: "Web Application", text: "This a web application i have made using react.js", imgURL: images.about01, github: "https://github.com/", live_site: "https://github.com/"},
+                                  {title: "Web Application", text: "This a web application i have made using react.js", imgURL: images.about01, github: "https://github.com/", live_site: "https://github.com/"},
+                                  {title: "Web Application", text: "This a web application i have made using react.js", imgURL: images.about01, github: "https://github.com/", live_site: "https://github.com/"},
+                                  {title: "Web Application", text: "This a web application i have made using react.js", imgURL: images.about01, github: "https://github.com/", live_site: "https://github.com/"},
+                                  {title: "Web Application", text: "This a web application i have made using react.js", imgURL: images.about01, github: "https://github.com/", live_site: "https://github.com/"}
+] },
+{ title: "C++", content: [{title: "Web Application", text: "This a web application i have made using react.js", imgURL: images.about01, github: "https://github.com/", live_site: "https://github.com/"},
+                                  {title: "Web Application", text: "This a web application i have made using react.js", imgURL: images.about01, github: "https://github.com/", live_site: "https://github.com/"},
+                                  {title: "Web Application", text: "This a web application i have made using react.js", imgURL: images.about01, github: "https://github.com/", live_site: "https://github.com/"},
+                                  {title: "Web Application", text: "This a web application i have made using react.js", imgURL: images.about01, github: "https://github.com/", live_site: "https://github.com/"},
+                                  {title: "Web Application", text: "This a web application i have made using react.js", imgURL: images.about01, github: "https://github.com/", live_site: "https://github.com/"}
+] },
+{ title: "Web App", content: [{title: "Web Application", text: "This a web application i have made using react.js", imgURL: images.about01, github: "https://github.com/", live_site: "https://github.com/"},
+                                  {title: "Web Application", text: "This a web application i have made using react.js", imgURL: images.about01, github: "https://github.com/", live_site: "https://github.com/"},
+                                  {title: "Web Application", text: "This a web application i have made using react.js", imgURL: images.about01, github: "https://github.com/", live_site: "https://github.com/"},
+                                  {title: "Web Application", text: "This a web application i have made using react.js", imgURL: images.about01, github: "https://github.com/", live_site: "https://github.com/"},
+                                  {title: "Web Application", text: "This a web application i have made using react.js", imgURL: images.about01, github: "https://github.com/", live_site: "https://github.com/"}
+] }
+  ];
+
+  export default data;

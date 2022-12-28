2e97313d10044685952273faa8a5e19256802929
Harshil Patel <26harshilpatel11@gmail.com>
2022-12-28 06:51:05 -0500

    Remove my_app and add all file and repo directly under this repo

--- my_app/src/components/index.js
+++
@@ -1,9 +0,0 @@
-import NavigationDot from './navigation';
-import SocialMedia from './socialMedia';
-import Navbar from './navbar/navbar';
-
-export {
-  NavigationDot,
-  SocialMedia,
-  Navbar,
-};

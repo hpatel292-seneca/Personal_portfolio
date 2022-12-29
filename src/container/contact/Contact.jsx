7280e190a1e8e7b7c0b2a7fb6967b48e6b39d976
Harshil Patel <26harshilpatel11@gmail.com>
2022-12-29 00:57:32 -0500

    Add contact page and add css for it

--- src/container/contact/Contact.jsx
+++ src/container/contact/Contact.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./contact.css"
 import "../../app.css"
 import AppWarp from '../../Higher_order_component/AppWapper'
 import { motion } from 'framer-motion'
 import { images } from '../../constant'
 function Contact() {
+    const [formData, setFormData] = useState({ name: '', email: '', message: '' })
+    const { username, email, message } = formData;
+    const [isFormSubmitted, setIsFormSubmitted] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const handleChangeInput = (e) => {
+        const [name, value] = e
+        setFormData({...formData, [name]: value})
+    }
+
+    const handleSubmit = () => {
+        setLoading(true);
+    
+        setIsFormSubmitted(true); 
+      };
     return (
         <motion.div className='app__container app__flex' id='contact'
             whileInView={{ y: [200, 0], opacity: [0, 1] }}
@@ -22,9 +36,32 @@ function Contact() {
                 </a>
             </div>
             <div className='app__form'>
-                <form action="#" method="post">
-                    <input type="text"  />
-                </form>
+                {!isFormSubmitted ?
+                    <>
+                        <div className='app__flex' >
+                            <input className="p-text" type="text" placeholder="Your Name" name="username" value={username} onChange={handleChangeInput} />
+                        </div>
+                        <div className='app__flex' >
+                            <input className="p-text" type="text" placeholder="Your Email" name="email" value={email} onChange={handleChangeInput} />
+                        </div>
+                        <div className='app__flex'>
+                            <textarea
+                                className="p-text textArea"
+                                placeholder="Your Message"
+                                value={message}
+                                name="message"
+                                onChange={handleChangeInput}
+                            />
+                        </div>
+                        <button type="button" className="submit-btn p-text app__flex" onClick={handleSubmit}>{!loading ? 'Send Message' : 'Sending...'}</button>
+                    </> : (
+                        <div>
+                            <h3 className="head-text">
+                                Thank you for getting in touch!
+                            </h3>
+                        </div>
+                    )
+                }
             </div>
         </motion.div>
     )

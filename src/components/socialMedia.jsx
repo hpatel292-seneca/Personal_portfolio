2e2bfcd321ef1223813fdd59bd331737ffd27a11
Harshil Patel <26harshilpatel11@gmail.com>
2024-07-10 15:38:12 -0400

    remove codepen logo and link from higher order component

--- src/components/socialMedia.jsx
+++ src/components/socialMedia.jsx
@@ -1,22 +1,21 @@
-import React from 'react'
-import {AiFillLinkedin, AiFillGithub, AiFillCodepenCircle} from "react-icons/ai"
-import "./socialMedia.css"
+import React from "react";
+import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
+import "./socialMedia.css";
 function SocialMedia() {
   return (
     <div className="app__social">
-
-        <div>
-          <a href='https://www.linkedin.com/in/harshil-patel-997b88219/'><AiFillLinkedin /></a>
-            
-        </div>
-        <div>    
-            <a href='https://codepen.io/trending'><AiFillCodepenCircle /></a>
-        </div>
-        <div>
-            <a href='https://github.com/hpatel292-seneca'><AiFillGithub /></a>
-        </div>
+      <div>
+        <a href="https://www.linkedin.com/in/harshil-patel-997b88219/">
+          <AiFillLinkedin />
+        </a>
+      </div>
+      <div>
+        <a href="https://github.com/hpatel292-seneca">
+          <AiFillGithub />
+        </a>
+      </div>
     </div>
-  )
+  );
 }
 
-export default SocialMedia
+export default SocialMedia;
